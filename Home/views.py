abba81025574e53d0780d3a42b3208ab80eacaf0
julyos2c <estosejuliusjess@gmail.com>
2026-02-04 10:25:17 +0800

    add api in homepage

--- Home/views.py
+++ Home/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'Home/home.html')
+    
+    url = "https://jsonplaceholder.typicode.com/posts"
+
+    response = requests.get(url)
+
+    posts = response.json()
+
+    return render(request, 'Home/home.html', {'posts' : posts})
+
+def about(request):
+    return render(request, 'Home/about.html')
+
+def contact(request):
+    return render(request, 'Home/contact.html')
